e16f917a009a5f4475fe78a7c612c1f0a24066f0
wal-india <keshav.b@westagilelabs.com>
2018-10-11 16:17:58 +0530

    Initial commit

---
+++ weather-report/app/components/Home.js
@@ -0,0 +1,19 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../constants/routes';
+import styles from './Home.css';
+
+type Props = {};
+
+export default class Home extends Component<Props> {
+  props: Props;
+
+  render() {
+    return (
+      <div className={styles.container} data-tid="container">
+        <Link to={routes.WEATHER} replace>Weather Link</Link>
+      </div>
+    );
+  }
+}
---
+++ weather-report/app/containers/WeatherPage.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+// import your child components
+import Weather from "../components/Weather";
+import {fetchWeatherData} from "../actions/weather"
+
+const mapStateToProps = state => {
+  return {
+    weatherData : state.weather,
+    cityName : state.city,
+    results: state.weather.results
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchWeather : async (event) => {
+      const results = await fetchWeatherData(event.target[0].value);
+      await dispatch(() => new Promise((resolve) => {
+          resolve(dispatch({ type: "PUSH_WEATHER_RESULTS", val: results.data }));
+        })
+      )
+    },
+    setSearchPhrase : (event) => dispatch({type: 'SET_SEARCH_PHRASE',val : event.target.value})
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Weather)
