44b8271d632c681b0c9b935fef35843873a058d0
Charani <sricharani@westagilelabs.com>
2018-10-15 19:35:56 +0530

    Initial commit

---
+++ News-client/app/models.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+let config = require("./config.json")
+const sequelize = new Sequelize("main", null, null,  {
+  dialect: 'sqlite',
+  storage: './database.db',
+  operatorsAliases: false
+});
+
+const NewsList = sequelize.define("news_list", {
+  author: Sequelize.TEXT,
+  source: Sequelize.TEXT,
+  title: Sequelize.TEXT,
+  image_url: Sequelize.TEXT,
+  source_url: Sequelize.TEXT,
+  created_date: Sequelize.DATEONLY,
+});
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
+const news_list = NewsList.build({author: '123', source: 'BBC', title: 'Testing'})
+news_list.save().then(() => {
+  console.log('news List created');
+})
+module.exports = {
+  sequelize: sequelize,
+  NewsList: NewsList
+}
