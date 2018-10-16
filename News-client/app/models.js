1c4c850806e4143f5d2f2bce4d82c92ce75c10bb
Sanjiv Kumar  Jha <doc@Sanjivs-MacBook-Pro.local>
2018-10-16 13:55:33 +0530

    Fix sqlite issue

--- News-client/app/models.js
+++ News-client/app/models.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 const sqlite3 = require('sqlite3')
-let config = require("./config.json")
+const path = require('path')
+const config = require("./config.json")
+
 const sequelize = new Sequelize("main", null, null,  {
   dialect: 'sqlite',
-  storage: './database.db',
+  storage: path.join(__dirname, '/database.db'),
   operatorsAliases: false
 });
 
@@ -14,18 +16,27 @@ const NewsList = sequelize.define("news_list", {
   image_url: Sequelize.TEXT,
   source_url: Sequelize.TEXT,
   created_date: Sequelize.DATEONLY,
+},{
+  timestamps: false,
 });
-let db = new sqlite3.Database('/Users/walindia/Documents/cl-demo/News-client/app/database.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the chinook database.');
-});
+// const _ = new sqlite3.Database('/Users/walindia/Documents/cl-demo/News-client/app/database.db', (err) => {
+//   if (err) {
+//     console.error(err.message);
+//   }
+//   console.log('Connected to the chinook database.');
+// });
 const news_list = NewsList.build({author: '123', source: 'BBC', title: 'Testing'})
 news_list.save().then(() => {
   console.log('news List created');
+  return 'aaaa'
 })
+.catch((err) => {
+  console.log('in promise catch')
+  console.log(err)
+})
+
+
 module.exports = {
-  sequelize: sequelize,
-  NewsList: NewsList
+  sequelize,
+  NewsList
 }
